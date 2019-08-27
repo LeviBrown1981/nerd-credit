abf0b793b8b8c0ca15885182258402a2322c081c
Levi Brown <rlevibrown@gmail.com>
2019-08-27 08:50:09 -0600

    final

---
+++ db/migrate/20190826222357_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.float :balance
+      t.string :avatar
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
